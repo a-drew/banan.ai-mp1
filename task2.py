2240da00b55759a576f3313036660b2c3e07df0d
Andrew H <andrew@plankdesign.com>
2021-10-16 01:45:35 -0400

    task 2: setup & run all classifiers in step 6

--- task2.py
+++ task2.py
@@ -7,8 +7,10 @@ import sklearn as skl
 import numpy as np
 import logging
 from sklearn import *
+from time import time
 
 # 2. Load the df in Python (you can use pandas.read csv)
+
 df = pd.read_csv('data/drug200.csv', dtype={'BP': 'category', 'Cholesterol': 'category', 'Drug': 'category'})
 print(df.dtypes)
 
@@ -19,10 +21,11 @@ classes = np.unique(y)
 print(x, y)
 
 '''
-# 3. Plot the distribution of the instances in each class and store the graphic in a file called drug-distribution.pdf.
-# You can use matplotlib.pyplot. This pre-analysis will allow you to determine if the classes are balanced,
-# and which metric is more appropriate to use to evaluate the performance of your classifier.
+3. Plot the distribution of the instances in each class and store the graphic in a file called drug-distribution.pdf.
+You can use matplotlib.pyplot. This pre-analysis will allow you to determine if the classes are balanced,
+and which metric is more appropriate to use to evaluate the performance of your classifier.
 '''
+
 counts = {drug: np.count_nonzero(y == drug) for drug in classes}
 print(counts)
 
@@ -43,37 +46,108 @@ x_numerical['BP'] = x_numerical['BP'].cat.codes
 x_numerical['Cholesterol'] = x_numerical['Cholesterol'].cat.codes
 y_numerical = y.cat.codes
 
-print(x_numerical)
-print(y_numerical)
+print(x_numerical, y_numerical)
 
 # 5. Split the df using train test split using the default parameter
 
-x_train, x_test, y_train, y_test = skl.model_selection.train_test_split(x_numerical, y_numerical)
+x_train, x_test, y_train, y_test = skl.model_selection.train_test_split(x_numerical, y)
+print(x_train, y_train)
 
-print(x_train)
-print(y_train)
+# 6. Run 6 different classifiers:
 
+# (a) NB: a Gaussian Naive Bayes Classifier (naive bayes.GaussianNB) with the default parameters.
+print("\nPerforming Gaussian NB training...")
+t0 = time()
+gnb = skl.naive_bayes.GaussianNB()
+gnb.fit(x_train, y_train)
+print("done in %0.3fs" % (time() - t0))
+print()
+
+# (b) Base-DT: a Decision Tree (tree.DecisionTreeClassifier) with the default parameters.
+print("Performing Base Decision Tree training...")
+t0 = time()
+bdt = skl.tree.DecisionTreeClassifier()
+bdt.fit(x_train, y_train)
+print("done in %0.3fs" % (time() - t0))
+print()
+
+# (c) Top-DT: a better performing Decision Tree found using (GridSearchCV). The gridsearch will allow
+# you to find the best combination of hyper-parameters, as determined by the evaluation function that
+# you have determined in step (3) above. The hyper-parameters that you will experiment with are:
+#    •criterion: gini or entropy
+#    •max depth : 2 different values of your choice
+#    •min samples split: 3 different values of your choice
+print("Performing Top-DT training w/ grid search...")
+parameters = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [4, 5],
+    'min_samples_split': [2, 5, 10]
+}
+print("parameters:", parameters)
+t0 = time()
+tdt = skl.model_selection.GridSearchCV(skl.tree.DecisionTreeClassifier(), parameters)
+tdt.fit(x_train, y_train)
+print("done in %0.3fs" % (time() - t0))
+print("Best score: %0.3f" % tdt.best_score_)
+best_params = tdt.best_estimator_.get_params()
+print("Best parameters:", {p: best_params[p] for p in parameters})
+print()
+
+# (d) PER: a Perceptron (linear model.Perceptron), with default parameter values.
+print("Performing Perceptron training...")
+t0 = time()
+per = skl.linear_model.Perceptron()
+per.fit(x_train, y_train)
+print("done in %0.3fs" % (time() - t0))
+print()
+
+# (e) Base-MLP: a Multi-Layered Perceptron (neural network.MLPClassifier) with 1 hidden layer of
+# 100 neurons, sigmoid/logistic as activation function, stochastic gradient descent, and default values
+# for the rest of the parameters.
+from sklearn.utils._testing import ignore_warnings
+
+print("Performing Base Multi-Layered Perceptron training...")
+t0 = time()
+bmlp = skl.neural_network.MLPClassifier(hidden_layer_sizes=100, activation='logistic', solver='sgd')
+#with ignore_warnings(category=skl.exceptions.ConvergenceWarning):
+bmlp.fit(x_train, y_train)
+print("done in %0.3fs" % (time() - t0))
+print()
+
+# (f) Top-MLP: a better performing Multi-Layered Perceptron found using grid search. For this, you need
+# to experiment with the following parameter values:
+#  •activation function: sigmoid, tanh, relu and identity
+#  •2 network architectures of your choice: for eg 2 hidden layers with 30 + 50 nodes, 3 hidden layers with 10 + 10 + 10
+#  •solver: Adam and stochastic gradient descent
+print("Performing Top-MLP training w/ grid search...")
+parameters = {
+    'hidden_layer_sizes': [(10, 20), (15, 15, 15)],
+    'activation': ['logistic', 'tanh', 'relu', 'identity'],
+    'solver': ['sgd', 'adam'],
+    'max_iter': [400],
+}
+print("parameters:", parameters)
+t0 = time()
+tmlp = skl.model_selection.GridSearchCV(skl.neural_network.MLPClassifier(), parameters)
+#with ignore_warnings(category=skl.exceptions.ConvergenceWarning):
+tmlp.fit(x_train, y_train)
+print("done in %0.3fs" % (time() - t0))
+print("Best score: %0.3f" % tmlp.best_score_)
+best_params = tmlp.best_estimator_.get_params()
+print("Best parameters:", {p: best_params[p] for p in parameters})
+print()
+
+print("Running predictions...")
+y_gnb = gnb.predict(x_test)
+y_bdt = bdt.predict(x_test)
+y_tdt = tdt.predict(x_test)
+y_per = per.predict(x_test)
+y_bmlp = bmlp.predict(x_test)
+y_tmlp = tmlp.predict(x_test)
+print()
+
+# TODO 7
 '''
-6. Run 6 different classifiers:
-(a) NB: a Gaussian Naive Bayes Classifier (naive bayes.GaussianNB) with the default parameters.
-(b) Base-DT: a Decision Tree (tree.DecisionTreeClassifier) with the default parameters.
-(c) Top-DT: a better performing Decision Tree found using (GridSearchCV). The gridsearch will allow
-you to find the best combination of hyper-parameters, as determined by the evaluation function that
-you have determined in step (3) above. The hyper-parameters that you will experiment with are:
-•criterion: gini or entropy
-•max depth : 2 different values of your choice
-•min samples split: 3 different values of your choice
-(d) PER: a Perceptron (linear model.Perceptron), with default parameter values.
-(e) Base-MLP: a Multi-Layered Perceptron (neural network.MLPClassifier) with 1 hidden layer of
-100 neurons, sigmoid/logistic as activation function, stochastic gradient descent, and default values
-for the rest of the parameters.
-(f) Top-MLP: a better performing Multi-Layered Perceptron found using grid search. For this, you need
-to experiment with the following parameter values:
-•activation function: sigmoid, tanh, relu and identity
-•2 network architectures of your choice: for eg 2 hidden layers with 30 + 50 nodes, 3 hidden layers
-with 10 + 10 + 10
-•solver: Adam and stochastic gradient descent
-
 7. For each of the 6 classifier above, append the following information in a file called drugs-performance.txt:
 (to make it easier for the TAs, make sure that your output for each sub-question below is clearly marked
 in your output file, using the headings (a), (b) . .. )
@@ -83,7 +157,10 @@ display the best hyperparameters found by the gridsearch.
 (b) the confusion matrix
 (c) the precision, recall, and F1-measure for each class
 (d) the accuracy, macro-average F1 and weighted-average F1 of the model
+'''
 
+# TODO 8
+'''
 8. Redo steps 6, 10 times for each model and append the average accuracy, average macro-average F1, average 
 weighted-average F1 as well as the standard deviation for the accuracy, the standard deviation of the macro-average F1, 
 and the standard deviation of the weighted-average F1 at the end of the file drugs-performance.txt. 
